0251cfbefae69435d4dd6e1236a48695bbcfed75
Quentin Hermiteau <quentinhermiteau@gmail.com>
2025-10-07 18:42:59 +0200

    ajout du sitemap, utilisation du composant <Image/>

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,20 @@
 import { NextConfig } from "next";
 import createNextIntlPlugin from "next-intl/plugin";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  images: {
+    // loader: "custom",
+    // customLoader: (src: string, width: number, quality: number) => {
+    //   return `${src}?w=${width}&q=${quality || 75}`;
+    // },
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "i.pinimg.com",
+      },
+    ],
+  },
+};
 
 const withNextIntl = createNextIntlPlugin();
 
